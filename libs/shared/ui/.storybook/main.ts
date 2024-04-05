c3750ed40d29b1e46ff30f0c06a2a4dd29e3e9b7
memoriaXII <chenglei.chou@gmail.com>
2024-04-06 00:43:32 +0800

    Add new UI component and update project configuration

--- libs/shared/ui/.storybook/main.ts
+++ libs/shared/ui/.storybook/main.ts
@@ -6,22 +6,22 @@ import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: [
-    
+
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'
   ],
   addons: ['@storybook/addon-essentials' , '@storybook/addon-interactions' ],
   framework: {
     name: '@storybook/react-vite',
     options: {
-      
+
     },
   },
-  
+
   viteFinal: async (config) =>
     mergeConfig(config, {
       plugins: [nxViteTsPaths()],
     }),
-  
+
 };
 
 export default config;
